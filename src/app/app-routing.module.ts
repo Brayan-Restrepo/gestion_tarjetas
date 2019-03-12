87905915b8e2234e842904c5fa57576aa7ab6f94
Brayan-Restrepo <brayan2293@gmail.com>
2019-03-11 21:02:14 -0500

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClienteComponent } from './pages/cliente/cliente.component';
+import { AsesoresComponent } from './pages/asesores/asesores.component';
 
 const routes: Routes = [
   {
     path: 'clientes',
     component: ClienteComponent
-  }
+  },
+  {
+    path: 'asesores',
+    component: AsesoresComponent
+  },
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+  { path: '**', redirectTo: 'clientes',  pathMatch: 'full' }
 ];
 
 @NgModule({
